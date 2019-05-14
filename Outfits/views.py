08d7343c78c4604ae3e71b043680c9ff70edb98f
Moiz Ahmed <moizz.ahmed@mail.utoronto.ca>
2019-05-14 18:53:04 -0400

    dashboard page complete

--- Outfits/views.py
+++ Outfits/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.views.generic import TemplateView
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login
+from .forms import SignUpForm
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'index.html'
