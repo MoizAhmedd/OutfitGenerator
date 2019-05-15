c62a8d77ce5a2733ab0faf237c7f9a5aef635a50
Moiz Ahmed <moizz.ahmed@mail.utoronto.ca>
2019-05-15 17:59:40 -0400

    creating clothing item table
    allows you to add a clothing item with a type, image, and name

--- OutfitGenerator/settings.py
+++ OutfitGenerator/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
+
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
--- Outfits/models.py
+++ Outfits/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class ClothingItem(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    category = models.CharField(max_length = 20)
+    name = models.CharField(max_length = 20)
+    image = models.ImageField(default = 'default.jpg',upload_to='clothes_pics')
+
+    def __str__(self):
+        return "{}'s {}".format(self.user.username,self.name)
+    
+
+
