b8f41d79d00d88348d510e19571f4b241aaf8a1d
Moiz Ahmed <moizz.ahmed@mail.utoronto.ca>
2019-05-15 19:14:20 -0400

    added resizing user uploaded images functionality using PIL

--- Outfits/models.py
+++ Outfits/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+from PIL import Image
 
 # Create your models here.
 class ClothingItem(models.Model):
@@ -11,5 +13,16 @@ class ClothingItem(models.Model):
     def __str__(self):
         return "{}'s {}".format(self.user.username,self.name)
     
+    def get_absolute_url(self):
+        return reverse('dashboard')
+    
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
 
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+        
 
