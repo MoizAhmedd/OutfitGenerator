291c3b138f7d959cb54d5061d5d8fdf60d89702a
Moiz Ahmed <43660046+MoizAhmedd@users.noreply.github.com>
2019-06-24 15:43:43 -0400

    prevent regeneration of disliked outfits

--- Outfits/views.py
+++ Outfits/views.py
@@ -4,9 +4,9 @@ from django.views.generic.edit import CreateView
 from django.contrib.auth.models import User
 from django.forms.models import model_to_dict
 from django.core import serializers
-from .models import ClothingItem
+from .models import ClothingItem, BadOutfit
 from django.contrib.auth import authenticate,login
-from .forms import SignUpForm
+from .forms import SignUpForm, ClothingItemForm
 from django.http import HttpResponse, HttpRequest,JsonResponse
 import random
 
@@ -24,7 +24,8 @@ class HomePageView(TemplateView):
             'hey':2
         })
 
- def generate_outfit(user):
+
+def generate_outfit(user):
     clothes = {}
     outfit = []
     for item in ClothingItem.objects.filter():
@@ -39,23 +40,33 @@ class HomePageView(TemplateView):
     return [outfit,len(clothes.values())]
 
 def DashboardView(request):
+    badoutfit = request.POST.get('Dislike')
     user = request.user
-    #print(user)
-    clothes = {}
-    outfit = []
-    for item in ClothingItem.objects.filter():
-        if item.category not in clothes:
-            if item.user == user:
-                clothes[item.category] = [item]
-        else:
-            if item.user == user:
-                clothes[item.category].append(item)
-    for cat in clothes:
-        outfit.append(random.choice(clothes[cat]))
-    insufficient = len(clothes.values()) < 2
-    #print('ye',outfit)
-    #Can't get data in JSON, however it can be rendered to display on template
-    return render(request,'dashboard.html',{'myclothes':outfit,'insufficient':insufficient})
+    outfit = generate_outfit(user)
+    badoutfits = {}
+    is_bad = True
+    insufficient = outfit[1] < 2
+
+    #Checks badoutfits
+    for count,outfits in enumerate(BadOutfit.objects.filter()):
+        badoutfits[count] = []
+        for item in outfits.items.all():
+            badoutfits[count].append(item)
+
+    #Adds to bad outfit model
+    if badoutfit == 'Dislike':
+        badoutfits = BadOutfit()
+        for clothing_item in outfit:
+            badoutfits.save()
+            badoutfits.items.add(clothing_item)
+    
+    count = len(badoutfits.values())
+    for wear in badoutfits.values():
+        while len(set(wear+outfit[0])) == len(outfit[0]):
+            #print('badone')
+            outfit = generate_outfit(user)
+
+    return render(request,'dashboard.html',{'myclothes':outfit[0],'insufficient':insufficient})
 
 class DashboardAPIView(APIView):
     renderer_classes = [TemplateHTMLRenderer]
@@ -95,13 +106,27 @@ class DashboardAPIView(APIView):
 
         #Is it possible to render a template, and to have data in json?
         
-        print('requested',outfit)
+        print('requested',outfit) 
         return Response({'myclothes':outfit,'insufficient':insufficient})
 
 class NewItemView(CreateView):
+
     model = ClothingItem
     template_name = 'newitem.html'
+    #fields = ['category','name','image']
     fields = '__all__'
+
+#Changed from class view
+def NewNotItemView(request):
+    if request.method == 'POST':
+        c_form = ClothingItemForm(request.POST,initial = {'user':request.user})
+        if c_form.is_valid():
+            c_form.save()
+            return redirect('dashboard')
+    else:
+        c_form = ClothingItemForm(initial = {'user':request.user,'image': 'default.jpg'})
+        print(c_form)
+    return render(request,'newitem.html',{'form':c_form})
 
 class MyClothesView(ListView):
     model = ClothingItem
