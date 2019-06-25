57f51fb11dd5a23b9149e08ab5af1a4eea20a5a7
Moiz Ahmed <43660046+MoizAhmedd@users.noreply.github.com>
2019-06-25 11:49:51 -0400

    register anticipateditem model

--- Outfits/admin.py
+++ Outfits/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import ClothingItem, BadOutfit
+from .models import ClothingItem, BadOutfit, AnticipatedItem
 # Register your models here.
 admin.site.register(ClothingItem)
 admin.site.register(BadOutfit)
+admin.site.register(AnticipatedItem)
