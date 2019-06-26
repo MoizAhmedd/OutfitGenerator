551f4d5e0ad8c6859a5852a1b3099d66ee3c844a
Moiz Ahmed <43660046+MoizAhmedd@users.noreply.github.com>
2019-06-26 15:07:11 -0400

    use GCP vision api to generate outfits

--- generate.py
+++ generate.py
@@ -1,1 +1,217 @@
-#Use transfer learning and Indico API to generate outfits based on a users wardrobe, and style preferences.
+from google.cloud import vision
+from google.oauth2 import service_account
+import os
+
+credentials = service_account.Credentials.from_service_account_file(r'C:\Users\ahmed\Documents\Summer2019\GCPCredentials\outfitgenerator-2e7110750cf6.json')
+def create_product_set(
+        project_id, location, product_set_id, product_set_display_name):
+    """Create a product set.
+    Args:
+        project_id: Id of the project.
+        location: A compute region name.
+        product_set_id: Id of the product set.
+        product_set_display_name: Display name of the product set.
+    """
+    client = vision.ProductSearchClient(credentials = credentials)
+
+    # A resource that represents Google Cloud Platform location.
+    location_path = client.location_path(
+        project=project_id, location=location)
+
+    # Create a product set with the product set specification in the region.
+    product_set = vision.types.ProductSet(
+            display_name=product_set_display_name)
+
+    # The response is the product set with `name` populated.
+    response = client.create_product_set(
+        parent=location_path,
+        product_set=product_set,
+        product_set_id=product_set_id)
+
+    # Display the product set information.
+    print('Product set name: {}'.format(response.name))
+    print('Product display name:{}'.format(response.display_name))
+
+def create_product(
+        project_id, location, product_id, product_display_name,
+        product_category):
+    """Create one product.
+    Args:
+        project_id: Id of the project.
+        location: A compute region name.
+        product_id: Id of the product.
+        product_display_name: Display name of the product.
+        product_category: Category of the product.
+    """
+    client = vision.ProductSearchClient(credentials=credentials)
+
+    # A resource that represents Google Cloud Platform location.
+    location_path = client.location_path(project=project_id, location=location)
+
+    # Create a product with the product specification in the region.
+    # Set product display name and product category.
+    product = vision.types.Product(
+        display_name=product_display_name,
+        product_category=product_category)
+
+    # The response is the product with the `name` field populated.
+    response = client.create_product(
+        parent=location_path,
+        product=product,
+        product_id=product_id)
+
+
+    # Display the product information.
+    print('Product name: {}'.format(response.display_name))
+
+def add_product_to_product_set(
+        project_id, location, product_id, product_set_id):
+    """Add a product to a product set.
+    Args:
+        project_id: Id of the project.
+        location: A compute region name.
+        product_id: Id of the product.
+        product_set_id: Id of the product set.
+    """
+    client = vision.ProductSearchClient(credentials=credentials)
+
+    # Get the full path of the product set.
+    product_set_path = client.product_set_path(
+        project=project_id, location=location,
+        product_set=product_set_id)
+
+    # Get the full path of the product.
+    product_path = client.product_path(
+        project=project_id, location=location, product=product_id)
+
+    # Add the product to the product set.
+    client.add_product_to_product_set(
+        name=product_set_path, product=product_path)
+    print('Product added to product set.')
+
+
+def update_product_labels(
+        project_id, location, product_id, key, value):
+    """Update the product labels.
+    Args:
+        project_id: Id of the project.
+        location: A compute region name.
+        product_id: Id of the product.
+        key: The key of the label.
+        value: The value of the label.
+    """
+    client = vision.ProductSearchClient(credentials=credentials)
+
+    # Get the name of the product.
+    product_path = client.product_path(
+        project=project_id, location=location, product=product_id)
+
+    # Set product name, product label and product display name.
+    # Multiple labels are also supported.
+    key_value = vision.types.Product.KeyValue(key=key, value=value)
+    product = vision.types.Product(
+        name=product_path,
+        product_labels=[key_value])
+
+    # Updating only the product_labels field here.
+    update_mask = vision.types.FieldMask(paths=['product_labels'])
+
+    # This overwrites the product_labels.
+    updated_product = client.update_product(
+        product=product, update_mask=update_mask)
+
+    # Display the updated product information.
+    print('Product name: {}'.format(updated_product.name))
+    print('Updated product labels: {}'.format(product.product_labels))
+
+
+def create_reference_image(
+        project_id, location, product_id, reference_image_id, gcs_uri):
+    """Create a reference image.
+    Args:
+        project_id: Id of the project.
+        location: A compute region name.
+        product_id: Id of the product.
+        reference_image_id: Id of the reference image.
+        gcs_uri: Google Cloud Storage path of the input image.
+    """
+    client = vision.ProductSearchClient(credentials = credentials)
+
+    # Get the full path of the product.
+    product_path = client.product_path(
+        project=project_id, location=location, product=product_id)
+
+    # Create a reference image.
+    reference_image = vision.types.ReferenceImage(uri=gcs_uri)
+
+    # The response is the reference image with `name` populated.
+    image = client.create_reference_image(
+        parent=product_path,
+        reference_image=reference_image,
+        reference_image_id=reference_image_id)
+
+    # Display the reference image information.
+    print('Reference image name: {}'.format(image.name))
+    print('Reference image uri: {}'.format(image.uri))
+
+def get_similar_products_file(
+        project_id, location, product_set_id, product_category,
+        file_path, filter):
+    """Search similar products to image.
+    Args:
+        project_id: Id of the project.
+        location: A compute region name.
+        product_set_id: Id of the product set.
+        product_category: Category of the product.
+        file_path: Local file path of the image to be searched.
+        filter: Condition to be applied on the labels.
+        Example for filter: (color = red OR color = blue) AND style = kids
+        It will search on all products with the following labels:
+        color:red AND style:kids
+        color:blue AND style:kids
+    """
+    # product_search_client is needed only for its helper methods.
+    product_search_client = vision.ProductSearchClient(credentials = credentials)
+    image_annotator_client = vision.ImageAnnotatorClient(credentials=credentials)
+
+    # Read the image as a stream of bytes.
+    with open(file_path, 'rb') as image_file:
+        content = image_file.read()
+
+    # Create annotate image request along with product search feature.
+    image = vision.types.Image(content=content)
+
+    # product search specific parameters
+    product_set_path = product_search_client.product_set_path(
+        project=project_id, location=location,
+        product_set=product_set_id)
+    product_search_params = vision.types.ProductSearchParams(
+        product_set=product_set_path,
+        product_categories=[product_category],
+        filter=filter)
+    image_context = vision.types.ImageContext(
+        product_search_params=product_search_params)
+
+    # Search products similar to the image.
+    response = image_annotator_client.product_search(
+        image, image_context=image_context)
+
+    index_time = response.product_search_results.index_time
+    print('Product set index time:')
+    print('  seconds: {}'.format(index_time.seconds))
+    print('  nanos: {}\n'.format(index_time.nanos))
+
+    results = response.product_search_results.results
+
+    print('Search results:')
+    for result in results:
+        product = result.product
+
+        print('Score(Confidence): {}'.format(result.score))
+        print('Image name: {}'.format(result.image))
+
+        print('Product name: {}'.format(product.name))
+        print('Product display name: {}'.format(
+            product.display_name))
+        print('Product description: {}\n'.format(product.description))
+        print('Product labels: {}\n'.format(product.product_labels))
