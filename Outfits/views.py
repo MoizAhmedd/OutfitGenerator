9eccb58ee61b75021ee0c76ddb8eb983adaf1157
Moiz Ahmed <43660046+MoizAhmedd@users.noreply.github.com>
2019-07-04 12:30:56 -0400

    determine if style preference chosen

--- Outfits/views.py
+++ Outfits/views.py
@@ -1,255 +1,239 @@
-from django.shortcuts import render,redirect
-from django.views.generic import TemplateView,ListView
-from django.views.generic.edit import CreateView
-from django.contrib.auth.models import User
-from django.forms.models import model_to_dict
-from django.core import serializers
-from .models import ClothingItem, BadOutfit, StyleOne, StyleTwo, StyleThree
-from django.contrib.auth import authenticate,login
-from .forms import SignUpForm, ClothingItemForm
-from django.http import HttpResponse, HttpRequest,JsonResponse
-import random
-from . import generate
-from rest_framework.renderers import TemplateHTMLRenderer
-from rest_framework.response import Response
-from rest_framework.views import APIView
-import os
+{% load staticfiles %}
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Dashboard</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{% static 'vendor/bootstrap/css/bootstrap.min.css' %}" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="{% static 'vendor/fontawesome-free/css/all.min.css' %}" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="{% static 'css/clean-blog.min.css' %}" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+
+    <!-- Page Header -->
+    <header class="masthead" style="background-image: url('/static/img/possible-head2.jpg')">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="page-heading">
+              <h1 style = "color: white !important">Hello {{user.username}}</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <!-- Post Content -->
+    <!-- Main Content -->
+    <div class="clearfix">
+      <a class="btn btn-primary float-right" href="{% url 'newitem' %}">Add Clothes</a>
+    </div>
+
+    <div class="clearfix">
+        <a class="btn btn-primary float-left " href="{% url 'myclothes' %}">View my Clothes/Outfits</a>
+    </div>
+
+    {% if not styleChosen %}
+      <h4> You have not chosen a style preference</h4>
+      <h4> Outfits are generated at random if you do not choose one </h4>
+      <div class="clearfix">
+          <center><a class="btn btn-primary" id = "choiceBtn" href="#">Choose Style Preference</a></center>
+      </div>
+    {% else %}
+    <div class="clearfix">
+        <center><a class="btn btn-primary" id = "choiceBtn" href="#">Change Style Preference</a></center>
+    </div>
+    {% endif %}
 
 
+    <div class="clearfix">
+        <center><a class="btn btn-primary" id = "myBtn" href="#">Generate an Outfit</a></center>
+    </div>
 
-# Create your views here.
-class HomePageView(TemplateView):
-    template_name = 'index.html'
-    def get(self,request):
-        return render(request,'index.html',{
-            'hey':2
-        })
-
-"""
-#Possible get similar products
-outfits = {}
-styleOne = request.POST.get('StyleOne')
-styleTwo = request.POST.get('StyleTwo')
-styleThree = request.POST.get('StyleThree')
-if styleOne:
-    for count, outfit in styleOne.objects.filter():
-        outfits[count] = []
-        for item in outfit.items.all(): #Get all clothing items in this outfit
-            outfits[count].append()
-            #This should be a clothing item
-                #Get the product name
-                #Find the id
-                #Go through the objects in users outfits
-                #If user object id matches product id
-                #Add to outfits[count]
-return [outfits[random.choice(len(outfits))],len(clothes.values())]
-"""
-def generate_gcp_outfit(user,style):
-    outfits = {}
-    objects = []
-    if style == 1:
-        objects = StyleOne.objects.filter()
-        #print(objects)
-    if style == 2:
-        objects = StyleTwo.objects.filter()
-    if style == 3:
-        objects = StyleThree.objects.filter()
-    for count, outfit in enumerate(objects):
-        outfits[count] = []
-        for item in outfit.items.all():
-            project_id = "outfitgenerator"
-            location = "us-east1"
-            product_set_id = user.username + str("_PS")
-            product_category = "apparel"
-            file_path = item.image.path
-            #print(product_set_id)
-            filter = "category=" + str(item.category)
-            #create clothing item from line below/find clothing item
-            #print(item)
-            product_id = generate.get_similar_products_file(project_id,location,product_set_id,product_category,file_path,filter)
-            for my_item in ClothingItem.objects.filter():
-                if my_item.user == user:
-                    if my_item.id == int(product_id):
-                        outfits[count].append(my_item)
-                        #print(my_item.name,my_item.id,product_id)
-                    #print(product_id,my_item.id)
-                #print(product_id,my_item.user,my_item.id)
-                #outfits[count].append(my_item)
-    #print(outfits[key])
-    try:
-        key = random.choice(list(outfits.keys()))
-        print(outfits[key])
-        return [outfits[key],len(outfits)]
-    except IndexError:
-        return [] 
-
-def insufficient_check(user):
-    num_items = 0
-    for item in ClothingItem.objects.filter():
-        if item.user == user:
-            num_items += 1
-    return num_items < 2
-
-
-def generate_outfit(user):
-    clothes = {}
-    outfit = []
-    for item in ClothingItem.objects.filter():
-        if item.category not in clothes:
-            if item.user == user:
-                clothes[item.category] = [item]
-        else:
-            if item.user == user:
-                clothes[item.category].append(item)
-    for cat in clothes:
-        outfit.append(random.choice(clothes[cat]))
-    return [outfit,len(clothes.values())]
-
-
-def DashboardView(request):
-    badoutfit = request.POST.get('Dislike')
-    StyleOne = request.POST.get('Alex Costa Outfits')
-    StyleTwo = request.POST.get('Alpha M Outfits')
-    StyleThree = request.POST.get('TMF Outfits')
-    #print(StyleTwo)
-    user = request.user
-    #generate_gcp_outfit(user)
-    #if StyleOne == 'StyleOne':
-    outfit = generate_gcp_outfit(user,1)
-    if StyleOne == "StyleOne":
-        outfit = generate_gcp_outfit(user,1)
-    if StyleTwo == "StyleTwo":
-        print('Alpha')
-        outfit = generate_gcp_outfit(user,2)
-    if StyleThree == "StyleThree":
-        print('TMF')
-        outfit = generate_gcp_outfit(user,3)
-    print(StyleOne,StyleTwo,StyleThree)
-    badoutfitslist = {}
-    is_bad = True
-    insufficient = insufficient_check(user)
-
-    #Checks badoutfits
-    for count,outfits in enumerate(BadOutfit.objects.filter()):
-        #badoutfitslist[count] = []
-        if outfits.user == user:
-            badoutfitslist[count] = []
-            for item in outfits.items.all():
-                badoutfitslist[count].append(item)
-
-    #Adds to bad outfit model
-    if badoutfit == 'Dislike':
-        badoutfits = BadOutfit()
-        for clothing_item in outfit[0]:
-            badoutfits.user = request.user
-            badoutfits.save()
-            badoutfits.items.add(clothing_item)
-                
-    #print(badoutfitslist)
-    count = len(badoutfitslist.values())
-    for wear in badoutfitslist.values():
-        if len(set(wear+outfit[0])) == len(outfit[0]) and len(badoutfitslist) != outfit:
-            #all items same, outfit is a bad one
-            #If length of badoutfits = length of total outfits, then insufficient
-            #if len(badoutfitslist) == outfit[1]:
-                #insufficient = True
-            #else:
-            if StyleOne == "StyleOne":
-                outfit = generate_gcp_outfit(user,1)
-            if StyleTwo == "StyleTwo":
-                outfit = generate_gcp_outfit(user,2)
-
-    if len(badoutfitslist) == outfit[1]:
-        return render(request,'dashboard.html',{'myclothes':outfit[0],'insufficient':True})
-    else:
-        return render(request,'dashboard.html',{'myclothes':outfit[0],'insufficient':insufficient})
-
-class DashboardAPIView(APIView):
-    renderer_classes = [TemplateHTMLRenderer]
-    template_name = 'result.html'
-    #parser_classes = []
-    def get(self,request):
-
-        """
-        Return a dictionary of an outfit, and if enough clothes
-        """
-        user = request.user
+    <div id = "myModal" class = "modal">
+      <div class = "modal-content">
+        <span class = "close">&times;</span>
         
-        clothes = {}
-        outfit = []
-        clothing_items = []
-        for item in ClothingItem.objects.filter():
-            clothing_items.append(item)
-        for item in clothing_items:
-            if item.category not in clothes:
-                if item.user == user:
-                    #print(model_to_dict(item))
-                    clothes[item.category] = [item]
-            else:
-                if item.user == user:
-                    clothes[item.category].append(item)
-        #print(clothes)
-        for cat in clothes:
-            item = random.choice(clothes[cat])
-            outfit.append(item)
-        #pot_outfit = serializers.serialize("json", outfit)
-        #print(outfit,pot_outfit)
-        #print(type(outfit[0]))
-        insufficient = len(clothes.values()) < 2
-        #print(model_to_dict(outfit[0]))
-        #Response class not working because outfit elements are not JSON serializable
-        #return HttpResponse({'myclothes':outfit,'insufficient':insufficient})
+        {% if insufficient %}
+          <center><h4>Could not generate outfit due to lack of clothes!</h4></center>
+          <div class="clearfix">
+            <center><a class="btn btn-primary" href="{% url 'newitem' %}">Click here to add Clothes</a></center>
+          </div>
+        {% else %}
+        <center><h2>Your Outfit!</h2></center>
+        {% for cloth in myclothes %}
+<!--Name--><h3 id="name">{{cloth.name}}</h3><!--EndName-->
+<!--Image--><img src = "{{MEDIA_URL}}{{cloth.image}}"><!--EndImage-->
+      {% endfor %}
+      <div class="clearfix">
+        <center><a class="btn btn-primary" id = "newBtn" href="#">Generate another</a></center>
+      </div>
 
-        #Is it possible to render a template, and to have data in json?
-        
-        print('requested',outfit) 
-        return Response({'myclothes':outfit,'insufficient':insufficient})
-
-class NewItemView(CreateView):
-
-    model = ClothingItem
-    template_name = 'newitem.html'
-    #fields = ['category','name','image']
-    fields = '__all__'
-
-#Changed from class view
-def NewNotItemView(request):
-    if request.method == 'POST':
-        c_form = ClothingItemForm(request.POST,initial = {'user':request.user})
-        if c_form.is_valid():
-            c_form.save()
-            return redirect('dashboard')
-    else:
-        c_form = ClothingItemForm(initial = {'user':request.user,'image': 'default.jpg'})
-        print(c_form)
-    return render(request,'newitem.html',{'form':c_form})
-
-class MyClothesView(ListView):
-    model = ClothingItem
-    #print(ClothingItem.objects.filter().first().user)
-    template_name = 'myclothes.html'
-    context_object_name = 'clothes'
+      <div class="clearfix">
+          <form action="" method="POST">
+              {% csrf_token %}
+              <center><input class = "btn btn-primary" type="submit" name="Dislike" value="Dislike" /></center>
+          </form>
+      </div>
+        {% endif %}
 
 
-def signup(request):
-    if request.user.is_authenticated:
-        return redirect('/')
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            project_id = "outfitgenerator"
-            location = "us-east1"
-            product_set_id = username + "_PS"
-            product_set_display_name = username + "_OUTFITS"
-            generate.create_product_set(project_id,location,product_set_id,product_set_display_name)
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username = username,password=raw_password)
-            login(request,user)
-            return redirect('/')
+      </div>
+    </div>
 
-    else:
-        form = SignUpForm()
+    <div id = "myModal1" class = "modal">
+      <div class = "modal-content">
+        <span class = "close">&times;</span>
+        <div class="clearfix">
+          <form action = "" method = "POST">
+            {% csrf_token %}
+            <img src = "{% static 'img/alexcosta.jpg' %}" height = "300" width = "300">
+            <input class  = "btn btn-primary" type = "submit" id = "Style1" name = "Alex Costa Outfits" value = "StyleOne">
+            <img src = "{% static 'img/alpham.jpg' %}" height = "300" width = "300">
+            <input class  = "btn btn-primary" type = "submit" id = "Style2" name = "Alpha M Outfits" value = "StyleTwo">
+            <img src = "{% static 'img/TMF.jpg' %}" height = "300" width = "300" >
+            <input class  = "btn btn-primary" type = "submit" id = "Style3" name = "TMF Outfits" value = "StyleThree">
+          </form>
+        </div>
+      </div>
+    </div>
+    
+    <div id = "loadingModal" class = "modal">
+      <div class = "modal-content">
+        <center><p>Please wait while we come up with outfits! May take 5 seconds to refresh page</p></center>
+      </div>
+    </div>
 
-    return render(request,'registration/signup.html',{'form':form})
+
+
+
+    <!-- Footer -->
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-10 mx-auto">
+
+            <p class="copyright text-muted">Copyright &copy; Sober Society</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="{% static 'vendor/jquery/jquery.min.js' %}"></script>
+    <script src="{% static 'vendor/bootstrap/js/bootstrap.bundle.min.js' %}"></script>
+    <script src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script>
+      // Get the modal
+        var modal = document.getElementById("myModal");
+        var choiceModal = document.getElementById("myModal1")
+        var loadingModal = document.getElementById("loadingModal")
+
+        // Get the button that opens the modal
+        var styleOne = document.getElementById("Style1")
+        var styleTwo = document.getElementById("Style2")
+        var styleThree = document.getElementById("Style3")
+        var btn = document.getElementById("myBtn");
+        var newBtn = document.getElementById("newBtn");
+        var choiceBtn = document.getElementById("choiceBtn")
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks the button, open the modal 
+        var clicked = false
+        styleOne.onclick = function(){
+          choiceModal.style.display = "none";
+          loadingModal.style.display = "block";
+        }
+        btn.onclick = function() {
+          alert("hey you clicked me")
+          modal.style.display = "block";
+        }
+
+        choiceBtn.onclick = function(){
+          alert("hey you clicked me")
+          choiceModal.style.display = "block";
+        }
+
+        newBtn.onclick = function(){
+          $.ajax({
+            method:"GET",
+            url: 'http://127.0.0.1:8000/dashboard',
+            success:function(data){
+              //First Name indices
+              modal.style.display = "none";
+              modal.style.display = "block";
+              // Stored all names in array
+              itemNames = []
+              //Store all images in Array
+              itemImages = []
+              //console.log(firstNewImage)
+              //console.log(secondNewImage)
+                //Change name and images of all items
+              lines = data.split('\n')
+              for (i=0;i<lines.length;i++){
+                if (lines[i].startsWith('<!--Name')){
+                  startingIndex = lines[i].indexOf('<h3')+14
+                  endingIndex = lines[i].indexOf('</h3>')
+                  itemNames.push(lines[i].slice(startingIndex,endingIndex))
+                } else if(lines[i].startsWith('<!--Image')){
+                  startingImageIndex = lines[i].indexOf('<img')
+                  endingImageIndex = lines[i].indexOf('<!--End')
+                  itemImages.push(lines[i].slice(startingImageIndex,endingImageIndex))
+                }
+              
+              }
+              for (i=0;i<itemNames.length;i++){
+                  document.getElementsByTagName('h3')[i].innerHTML = itemNames[i]
+                  document.getElementsByTagName('img')[i].outerHTML = itemImages[i]
+              }
+              console.log(itemImages)
+            },
+            failure: function(data){
+              console.log('fail bro')
+            }
+          })
+          //modal.style.display = "block";
+        }
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+          modal.style.display = "none";
+          choiceModal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+          if (event.target == choiceModal){
+            choiceModal.style.display = "none";
+          }
+        }
+    </script>
+  </body>
+
+</html>
