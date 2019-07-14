62f2591b29039cf93b700b100ccbb5618711025c
Moiz Ahmed <43660046+MoizAhmedd@users.noreply.github.com>
2019-07-14 18:27:41 -0400

    urls finalized

--- Outfits/urls.py
+++ Outfits/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',views.HomePageView.as_view(),name='home'),
     path('login/',auth_views.LoginView.as_view(redirect_authenticated_user=True),name='login'),
-    #path('meh',views.CheckUserView,name='checkuser'),
     path('signup/',views.signup,name='signup'),
     path('logout/',auth_views.LogoutView.as_view(),name='logout'),
     path('dashboard/<int:style>',views.DashboardView,name='dashboard'),
