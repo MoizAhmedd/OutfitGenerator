b834f4918cfbebb94623dd92102df66e60b9f1b9
Moiz Ahmed <43660046+MoizAhmedd@users.noreply.github.com>
2019-07-14 18:30:20 -0400

    all final models registered

--- Outfits/admin.py
+++ Outfits/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import ClothingItem, BadOutfit, AnticipatedItem
+from .models import ClothingItem, BadOutfit,PossibleItem, StyleOne, StyleTwo, StyleThree
 # Register your models here.
 admin.site.register(ClothingItem)
 admin.site.register(BadOutfit)
-admin.site.register(AnticipatedItem)
+admin.site.register(PossibleItem)
+admin.site.register(StyleOne)
+admin.site.register(StyleTwo)
+admin.site.register(StyleThree)
+
